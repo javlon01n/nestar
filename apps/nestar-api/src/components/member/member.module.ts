888811901227056451fb9560d186b0f970bb452f
MARK <joxa030@gmail.com>
2026-08-20 10:11:27 +0900

    feat: hashing password via bcryptjs

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -3,9 +3,10 @@ import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
+import { AuthService } from '../auth/auth.service';
 
 @Module({
 	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }])],
-	providers: [MemberResolver, MemberService],
+	providers: [MemberResolver, MemberService, AuthService],
 })
 export class MemberModule {}
