064830c871750dd4e26382bc2495ef6ec97c01e8
MARK <joxa030@gmail.com>
2026-08-31 23:04:31 +0900

    feat: develop feat: develop updateProperty  business logic

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -10,6 +10,8 @@ import { MemberService } from '../member/member.service';
 import { PropertyStatus } from '../../libs/enums/property.enum';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { T, StatisticModifier } from '../../libs/types/common';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
+import moment from 'moment';
 
 @Injectable()
 export class PropertyService {
@@ -61,9 +63,40 @@ export class PropertyService {
 		return targetProperty;
 	}
 
-	public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
-		const { _id, targetKey, modifier } = input;
-		return await this.propertyModel.findByIdAndUpdate(_id, { $inc: { [targetKey]: modifier } },
-             { new: true }).exec();
-	}
+        public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
+            const { _id, targetKey, modifier } = input;
+            return await this.propertyModel.findByIdAndUpdate(_id, { $inc: { [targetKey]: modifier } },
+                { new: true }).exec();
+        }
+
+        
+        public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property> {
+        let { propertyStatus, soldAt, deletedAt } = input;
+        const search: T = {
+            _id: input._id,
+            memberId: memberId,
+            propertyStatus: PropertyStatus.ACTIVE,
+        };
+
+        if (propertyStatus === PropertyStatus.SOLD) soldAt = moment().toDate();
+        else if (propertyStatus === PropertyStatus.DELETE) deletedAt = moment().toDate();
+
+        const result = await this.propertyModel
+            .findOneAndUpdate(search, input, {
+                new: true,
+            })
+            .exec();
+        if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+        if (soldAt || deletedAt) {
+            await this.memberService.memberStatsEditor({
+                _id: memberId,
+                targetKey: 'memberProperties',
+                modifier: -1,
+            });
+        }
+
+        return result;
+    }
+
 }
