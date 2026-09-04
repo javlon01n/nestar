c3000e280a396618707761d8703b8e06d46d20f3
MARK <joxa030@gmail.com>
2026-09-05 08:10:33 +0900

    fix: modifiy getMember business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -13,11 +13,14 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
 import { LikeService } from '../like/like.service';
+import { Follower, Following, MeFollowed } from '../../libs/dto/follow/follow';
 
 @Injectable()
 export class MemberService {
     [x: string]: any;
-	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>,
+	constructor(
+        @InjectModel('Member') private readonly memberModel: Model<Member>,
+        @InjectModel('Follow') private readonly followModel: Model<Follower | Following>,
     private authService: AuthService,
     private viewService: ViewService,
     private likeService: LikeService,
@@ -89,11 +92,17 @@ export class MemberService {
         const likeInput = { memberId: memberId, likeRefId: targetId, likeGroup: LikeGroup.MEMBER };
         targetMember.meLiked = await this.likeService.checkLikeExistence(likeInput);
 
-        // meFollowed
+        targetMember.meFollowed = await this.checkSubscription(memberId, targetId);
         }
 
         return targetMember;
     }
+
+        private async checkSubscription(followerId: ObjectId, followingId: ObjectId): Promise<MeFollowed[]> {
+        const result = await this.followModel.findOne({ followingId: followingId, followerId: followerId }).exec();
+        return result ? [{ followerId: followerId, followingId: followingId, myFollowing: true }] : [];
+    }
+
 
         public async getAgents(memberId: ObjectId, input: AgentsInquiry): Promise<Members> {
         const { text } = input.search;
@@ -172,7 +181,6 @@ export class MemberService {
         if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
         return result;
     }
-
 
         public async memberStatsEditor(input: StatisticModifier): Promise<Member> {
         const { _id, targetKey, modifier } = input;
