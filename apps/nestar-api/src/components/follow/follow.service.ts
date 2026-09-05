215bb7a8e3cd196ad880f4bad4e781d08fd1e85e
MARK <joxa030@gmail.com>
2026-09-05 15:04:30 +0900

    feat: integration of lookupAuthMemberFollowed  complex query business logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -6,7 +6,8 @@ import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { T } from '../../libs/types/common';
-import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberFollowed, lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { Member } from '../../libs/dto/member/member';
 
 @Injectable()
 export class FollowService {
@@ -75,7 +76,10 @@ export class FollowService {
                             { $skip: (page - 1) * limit },
                             { $limit: limit },
                             lookupAuthMemberLiked(memberId, "$followingId"),
-                            // meFollowed
+                            lookupAuthMemberFollowed({
+                                followerId: memberId,
+                                followingId: "$followingId", 
+                            }),
                             lookupFollowingData,
                             { $unwind: '$followingData' },
                         ],
@@ -106,7 +110,10 @@ export class FollowService {
                             { $skip: (page - 1) * limit },
                             { $limit: limit },
                             lookupAuthMemberLiked(memberId, "$followerId"),
-                            // meFollowed
+                            lookupAuthMemberFollowed({
+                                followerId: memberId,
+                                followingId: "$followerId", 
+                            }),
                             lookupFollowerData,
                             { $unwind: '$followerData' },
                         ],
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -63,6 +63,43 @@ export const shapeIntoMongoObjectId = (target: any) => {
 		};
 	};
 
+	interface LookupAuthMemberFollowed {
+	followerId: T;
+	followingId: string;
+ }
+
+	export const lookupAuthMemberFollowed = (input: LookupAuthMemberFollowed) => {
+		const { followerId, followingId } = input;
+		return {
+			$lookup: {
+				from: 'follows',
+				let: {
+					localFollowerId: followerId,
+					localFollowingId: followingId,
+					localMyFavorite: true,
+				},
+				pipeline: [
+					{
+						$match: {
+							$expr: {
+								$and: [{ $eq: ['$followerId', '$$localFollowerId'] }, { $eq: ['$followingId', '$$localFollowingId'] }],
+							},
+						},
+					},
+					{
+						$project: {
+							_id: 0,
+							followerId: 1,
+							followingId: 1,
+							myFollowing: '$$localMyFavorite',
+						},
+					},
+				],
+				as: 'meFollowed',
+			},
+		};
+	};
+
 	export const lookupMember = {
 		$lookup: {
 			from: 'members',
